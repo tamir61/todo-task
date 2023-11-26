ad9da6d2af0df38e14042258b1a96bf504652539
tamir.halima <tamir.halima@appsflyer.com>
2023-11-26 20:02:22 +0200

    setup husky, eslint and prettier. project is ready for development

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -8,6 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
-
